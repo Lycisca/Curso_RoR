4dbc1807ba3532f72da24e17dd7f8fd67e9661f7
Nuria <nury@mint>
2013-05-09 20:43:27 +0200

    Crear app rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cursofer::Application.config.secret_token = 'bafa36c762a4175835b23aa08690bd54d9a0ecf598932f567cc1760b7fce40ad5ed17bc06d520110f77c4a045c5dcce1bcaed29850755cd3e39c15250789dd7d'
